cd19b4985295831cd2835cab3973347c3fb84a91
Raff_12 <raffelecoppola381@gmail.com>
2025-03-14 21:08:49 +0100

    feat: Prova e Integrazione dell'Api per scopo di risposta

--- Milestone - 1/Day-2 - Implementazione Finale Dello Stile/app.js
+++ Milestone - 1/Day-2 - Implementazione Finale Dello Stile/app.js
@@ -1,8 +1,9 @@
 // Scopo del Progetto:
-// Realizzazione di Galleria di immagini con il js in questione.
+// Realizzazione di Galleria di immagini con il js utilizzando un api.
+// Basato sulla creazione degli elementi dettati dallo script nel DOM.
 
 let HandleWindowShow = ()=>{
-    return window.alert("Benvenuto nella Pagina di questa galleria, Buona visita.");
+    return window.alert("Benvenuto nella Pagina di questa galleria, ti auguro una buona visita.");
 }
 
 HandleWindowShow();
--- Milestone - 2/app.js
+++ Milestone - 2/app.js
@@ -1,12 +1,38 @@
 // Scopo del Progetto:
-// Realizzazione di Galleria di immagini con il js in questione.
+// Realizzazione di Galleria di immagini con il js utilizzando un api.
+// Basato sulla creazione degli elementi dettati dallo script nel DOM.
+
+/**
+ * Indice dei Componenti dello script:
+ *
+ * @type {"https://lanciweb.github.io/demo/api/pictures/"} - URL dell'Api per la risposta AJAX.
+ * 
+ * @var {"parent_node"} - Variabile di contenimento sull elemento "#parent-node".
+ * 
+ * @function {"HandleApiGenResponse"} - Funzione di chiamata Api con il metodo fetch().
+ * 
+ */
 
 const url = "https://lanciweb.github.io/demo/api/pictures/";
-let parent_node = document.getElementById("root");
+let parent_node = document.getElementById("parent-node");
 
 async function HandleApiGenResponse(){
-    let response = fetch(url).then(response => response.json());
-    return response
+    let response = await fetch(url);
+    let data_obj = await response.json();
+    console.log("Risposta Dell'Api:", data_obj);
+    let title = []
+    // Prova di Creazione di risposta API - per scopo di verifica e implementazione.
+    setTimeout(()=>{
+        for (let index = 0; index < 6; index++) {
+            title.push(data_obj[index].title);
+        // Prova di Interazione con il DOM con L'API.
+            let text_node = document.createTextNode(title[index]);
+            let create_li = document.createElement("li");
+            create_li.appendChild(text_node);
+            parent_node.appendChild(create_li);
+        }
+    }, 1100)
+    return parent_node
 }
 
-console.log(HandleApiGenResponse());
+HandleApiGenResponse();
