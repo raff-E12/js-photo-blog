f87abcdfe2b9184d72e395aa4e87ffc852f3cdd0
Raff_12 <raffelecoppola381@gmail.com>
2025-03-14 16:40:53 +0100

    Feat: Creazione della struttura di base del progetto e disposizione delle fasi creative

---
+++ Day-1 -  Creazione della Struttura Generale/app.js
@@ -0,0 +1,8 @@
+// Scopo del Progetto:
+// Realizzazione di Galleria di immagini con il js in questione.
+
+let HandleWindowShow = ()=>{
+    return window.alert("Benvenuto nella Pagina di questa galleria, Buona visita.");
+}
+
+HandleWindowShow();
