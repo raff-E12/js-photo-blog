7bd8ca0d6ebe3923e49c978a8c4b1adb1d542e26
Raff_12 <raffelecoppola381@gmail.com>
2025-03-14 21:38:18 +0100

    feat: Aggiunta di commento nello script

--- Milestone - 2/app.js
+++ Milestone - 2/app.js
@@ -16,6 +16,7 @@
 const url = "https://lanciweb.github.io/demo/api/pictures/";
 let parent_node = document.getElementById("parent-node");
 
+// Oggetto di risposta all Api.(Estrazione dei dati in maniera rapida).
 let gallery_obj = [
     {id: 1, title: "Skate Park", date: "01-07-2024", url: "https://marcolanci.it/boolean/assets/pictures/1.png"},
     {id: 2, title: "Passeggiata", date: "16-07-2024", url: "https://marcolanci.it/boolean/assets/pictures/2.png"},
