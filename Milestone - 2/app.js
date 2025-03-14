60f9f0960b544786c5ef0c5c9a19b240025e95c1
Raff_12 <raffelecoppola381@gmail.com>
2025-03-14 21:31:47 +0100

    feat: Miglioramento del Codice di Prova

--- Milestone - 2/app.js
+++ Milestone - 2/app.js
@@ -16,16 +16,30 @@
 const url = "https://lanciweb.github.io/demo/api/pictures/";
 let parent_node = document.getElementById("parent-node");
 
+let gallery_obj = [
+    {id: 1, title: "Skate Park", date: "01-07-2024", url: "https://marcolanci.it/boolean/assets/pictures/1.png"},
+    {id: 2, title: "Passeggiata", date: "16-07-2024", url: "https://marcolanci.it/boolean/assets/pictures/2.png"},
+    {id: 3, title: "Alpi", date: "01-07-2024", url: "https://marcolanci.it/boolean/assets/pictures/3.png"},
+    {id: 4, title: "Sagra", date: "21-08-2024", url: "https://marcolanci.it/boolean/assets/pictures/4.png"},
+    {id: 5, title: "Watergun", date: "23-08-2024", url: "https://marcolanci.it/boolean/assets/pictures/5.png"},
+    {id: 6, title: "Riviera", date: "30-08-2024", url: "https://marcolanci.it/boolean/assets/pictures/6.png"},
+];
+
 async function HandleApiGenResponse(){
     let response = await fetch(url);
     let data_obj = await response.json();
     console.log("Risposta Dell'Api:", data_obj);
-    let title = []
+    // Destruturazione del Array di oggetti
+    let title = gallery_obj.map(({title}) => title);
+    let data = gallery_obj.map(({date}) => date);
+    let url_list = gallery_obj.map(({url}) => url);
+    console.log(title);
+    console.log(data);
+    console.log(url_list);
     // Prova di Creazione di risposta API - per scopo di verifica e implementazione.
     setTimeout(()=>{
         for (let index = 0; index < 6; index++) {
-            title.push(data_obj[index].title);
-        // Prova di Interazione con il DOM con L'API.
+        // // Prova di Interazione con il DOM con L'API.
             let text_node = document.createTextNode(title[index]);
             let create_li = document.createElement("li");
             create_li.appendChild(text_node);
