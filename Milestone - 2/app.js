282e41eae8ed53b81c2daa77748f916f41881961
Raff_12 <raffelecoppola381@gmail.com>
2025-03-14 18:28:39 +0100

    feat: Implementazione dell'Api

--- Milestone - 2/app.js
+++ Milestone - 2/app.js
@@ -1,2 +1,12 @@
 // Scopo del Progetto:
 // Realizzazione di Galleria di immagini con il js in questione.
+
+const url = "https://lanciweb.github.io/demo/api/pictures/";
+let parent_node = document.getElementById("root");
+
+async function HandleApiGenResponse(){
+    let response = fetch(url).then(response => response.json());
+    return response
+}
+
+console.log(HandleApiGenResponse());
